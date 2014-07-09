72ef5c172dc4e050b80b456b88571e58497828e9
Jonathan Reem <jonathan.reem@gmail.com>
2014-07-09 13:47:23 -0700

    (feat) Scaffold after structure
    Set up matching on 'Accept-Encoding' header and pass on res if there is
    no such header.

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,24 @@
 extern crate conduit;
 extern crate middleware = "conduit-middleware";
 
+use middleware::Middleware;
+use conduit::{Request, Response};
+
+#[deriving(Show, Clone)]
+struct Compress;
+
+impl middleware::Middleware for Compress {
+    fn after(&self, req: &mut Request, res: Result<Response, Box<Show>>)
+        -> Result<Response, Box<Show>> {
+        match req.headers.find("Accept-Encoding") {
+            Some(ref accept_encoding) => {
+                res.map(|r| {
+                    // do some compression
+                })
+            },
+            // no compression enabled for this request
+            None => res
+        }
+    }
+}
+
